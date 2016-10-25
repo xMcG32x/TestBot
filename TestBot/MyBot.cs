35df2ca50792a8fbe119514b67dbc843cefc80e8
Mike.McGinness <Mike.McGinness@MM201034L5>
2016-10-25 09:30:23 -0400

    Add project files.

---
+++ TestBot/MyBot.cs
@@ -0,0 +1,93 @@
+﻿/**********************************************************************************************************************
+/   Discord Test Bot
+/   Created by Mike
+/   Date: 10/21/2016
+/   Purpose:  To test how a bot works in Discord chat and get a basic understanding of all the features.
+/
+/**********************************************************************************************************************/
+
+using Discord;
+using Discord.Commands;
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot
+{
+
+    //MyBot Class which was delcared in the Program Class
+    class MyBot
+    {
+        //Declaration of variables through class
+        DiscordClient discord;
+        CommandService commands;
+        Random rand;
+         
+
+        //Public instance of the MyBot class
+        public MyBot()
+        {
+            rand = new Random();
+            
+            //Logs for actions taken in Discord client.  Will display in console window.               
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });                             
+           
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MjM5MDIxNTQxMjA2MTMwNjg4.Cuu9Qg.SneOzJQylShGfAdOJRSvVdP8Uwg", TokenType.Bot);
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            commands = discord.GetService<CommandService>();
+
+            RegisterAdviceCommand();
+
+            commands.CreateCommand("Hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Fuck Off!!");
+                });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private void RegisterAdviceCommand()
+        {
+            const string f = "advicedarkest.txt";
+            List<string> lines = new List<string>();            
+
+            using (StreamReader r = new StreamReader(f))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            string[] a = lines.ToArray();
+
+            commands.CreateCommand("advice")
+               .Do(async (e) =>
+               {
+                   int randomAdviceIndex = rand.Next(a.Length);
+                   await e.Channel.SendMessage(a[randomAdviceIndex]);
+               });
+        }
+    }
+}
---
+++ TestBot/Program.cs
@@ -0,0 +1,25 @@
+﻿/**********************************************************************************************************************
+/   Discord Test Bot
+/   Created by Mike
+/   Date: 10/21/2016
+/   Purpose:  To test how a bot works in Discord chat and get a basic understanding of all the features.
+/
+/**********************************************************************************************************************/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+        }                     
+    }
+}
